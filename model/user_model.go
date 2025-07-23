4dcde91e4754b8f263fbdb7a1f69aed57977ddf3
yeanSinizter <about.wswyeanbkjn@gmail.com>
2025-07-23 10:56:57 +0700

    fix:struct user

--- model/user_model.go
+++ model/user_model.go
@@ -1,17 +1,17 @@
 package model
 
-import "time"
+import (
+	"gorm.io/gorm"
+)
 
 type User struct {
-	Id             string    `gorm:"primaryKey;type:varchar(50)" json:"id" example:"1"`
-	UserName       string    `gorm:"type:varchar(50)" json:"userName" example:"john.doe"`
-	Email          string    `gorm:"type:varchar(100)" json:"email" example:"user@example.com"`
-	RoleId         string    `gorm:"type:varchar(50)" json:"roleId" example:"ROLE_ADMIN"`
-	Team           string    `gorm:"type:varchar(50)" json:"team" example:"CEN123456"`
-	CenterId       string    `gorm:"type:varchar(50)" json:"centerId" example:"Team A"`
-	IsActive       string    `gorm:"type:varchar(50)" json:"isActive" example:"true"`
-	CreateDatetime time.Time `gorm:"type:timestamp" json:"createDatetime" example:"2025-07-17T09:00:00Z"`
-	UpdateDatetime time.Time `gorm:"type:timestamp" json:"updateDatetime" example:"2025-07-17T09:00:00Z"`
+	gorm.Model
+	UserName string `gorm:"type:varchar(50)" json:"userName" example:"john.doe"`
+	Email    string `gorm:"type:varchar(100)" json:"email" example:"user@example.com"`
+	RoleId   string `gorm:"type:varchar(50)" json:"roleId" example:"ROLE_ADMIN"`
+	Team     string `gorm:"type:varchar(50)" json:"team" example:"CEN123456"`
+	CenterId string `gorm:"type:varchar(50)" json:"centerId" example:"Team A"`
+	IsActive string `gorm:"type:varchar(50)" json:"isActive" example:"true"`
 }
 
 func (User) TableName() string {
--- test/integration/mock/mock_repository.go
+++ test/integration/mock/mock_repository.go
@@ -2,9 +2,11 @@ package mock
 
 import (
 	"case-management/model"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 type MockRepository struct{}
@@ -12,24 +14,39 @@ type MockRepository struct{}
 func (m *MockRepository) GetAllUsers(c *gin.Context) ([]*model.User, error) {
 	return []*model.User{
 		{
-			Id:             "1234",
-			UserName:       "john_doe",
-			Email:          "john.doe@example.com",
-			RoleId:         "1",
-			Team:           "Inbound",
-			IsActive:       "ACTIVE",
-			CreateDatetime: time.Now(),
-			UpdateDatetime: time.Now(),
+			Model: gorm.Model{
+				ID:        1,
+				CreatedAt: time.Now(),
+				UpdatedAt: time.Now(),
+			},
+			UserName: "john_doe",
+			Email:    "john.doe@example.com",
+			RoleId:   "1",
+			Team:     "Inbound",
+			CenterId: "CEN001",
+			IsActive: "ACTIVE",
 		},
 	}, nil
 }
 
 func (m *MockRepository) GetUserByID(c *gin.Context, id string) (*model.User, error) {
-	return &model.User{Id: id, UserName: "Mock_User", Email: "mock.user@example.com"}, nil
+	uid, err := strconv.ParseUint(id, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	return &model.User{
+		Model:    gorm.Model{ID: uint(uid)},
+		UserName: "Mock_User",
+		Email:    "mock.user@example.com",
+		RoleId:   "1",
+		Team:     "Inbound",
+		CenterId: "Center001",
+		IsActive: "ACTIVE",
+	}, nil
 }
 
-func (m *MockRepository) CreateUser(c *gin.Context, user *model.User) (string, error) {
-	return user.Id, nil
+func (m *MockRepository) CreateUser(c *gin.Context, user *model.User) (uint, error) {
+	return user.ID, nil
 }
 
 func (m *MockRepository) DeleteUserByID(c *gin.Context, id string) error {
